8c6053d8a240f32f7ec8e171fa5eabdec3bb5837
ishmeet-grayopusq <120010191+ishmeet-grayopusq@users.noreply.github.com>
2023-02-23 02:53:55 +0530

    Historical data processing completed

--- main.py
+++ main.py
@@ -1,24 +1,74 @@
 import os
+import json
 import uvicorn
-from fastapi import FastAPI, Request
-from product_data_handler import process_all_urls, single_product_data_fetcher
+from fastapi import FastAPI, Request, Body
+from models import ProductInfo
+from product_data_handler import (
+    process_all_urls,
+    single_product_data_fetcher,
+    process_historical_data,
+)
 
 app = FastAPI()
 
 
+@app.get("/")
+async def root():
+    return {"app_status": "healthy"}
+
+
+@app.get("/product_info")
+async def product_info():
+    """
+    Returns a list of products that we can process as of now.
+    """
+    with open("Data/available_products.json", "r") as file_obj:
+        product_data = file_obj.read()
+    return json.loads(product_data)
+
+
 @app.get("/all_products")
 async def all_url_processor():
+    """
+    Returns info about current prices of all products.
+    """
     return process_all_urls()
 
 
-@app.get("/single_product")
-async def single_product_processor(request: Request):
-    body = await request.json()
-    return single_product_data_fetcher(body['Product_URL'])
+@app.post("/single_product")
+async def single_product_processor(
+    requested_product: ProductInfo = Body(example={"Product_Name": "Vega Atom Helmet"}),
+):
+    """
+    Extracts and returns info about 1 product from Amazon and Flipkart.
+    """
+    with open("Data/available_products.json", "r") as file_obj:
+        product_data = json.loads(file_obj.read())
+    return await single_product_data_fetcher(
+        requested_product.Product_Name, product_data[requested_product.Product_Name]
+    )
+
+
+@app.post("/historical_data")
+async def historical_data_generator(
+    requested_product: ProductInfo = Body(
+        example={
+            "Product_Name": "Vega Atom Helmet",
+        }
+    ),
+):
+    """
+    Generates historical data for 1 product from Amazon and Flipkart.
+    """
+    with open("Data/available_products.json", "r") as file_obj:
+        product_data = json.loads(file_obj.read())
+    return await process_historical_data(
+        requested_product.Product_Name, product_data[requested_product.Product_Name]
+    )
 
 
 if __name__ == "__main__":
     directories = os.listdir()
-    if 'Data' not in directories:
+    if "Data" not in directories:
         os.mkdir("Data")
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
--- product_data_handler.py
+++ product_data_handler.py
@@ -1,118 +1,301 @@
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
+from datetime import datetime
 from pprint import pprint
 import pandas as pd
 import requests
+import asyncio
 import json
 import re
 
 
-def single_product_data_fetcher(url: str):
+async def amazon_processing(product_name, headers, url):
+    # Making the HTTP Request
+    webpage = requests.get(url, headers=headers)
 
-	# to extract data from a script, we need to create a User-Agent as a has blocked robots from accessing data
-	user_agent = UserAgent()
-	headers = ({"User-Agent": user_agent.random})
+    # Creating the Soup Object containing all the data
+    soup = BeautifulSoup(webpage.content, "lxml")
 
-	x = re.search("amazon", url)
-	try:
-		if x is None:
-			print("flipkart link found!")
-			print(url)
-		else:
-			# Making the HTTP Request
-			print("Amazon link found!")
-			webpage = requests.get(url, headers=headers)
+    # retrieving product title
+    try:
+        # Outer Tag Object
+        title = soup.find("span", attrs={"id": "productTitle"})
 
-			# Creating the Soup Object containing all the data
-			soup = BeautifulSoup(webpage.content, "lxml")
+        # Inner NavigableString Object
+        title_value = title.string
 
-			# retrieving product title
-			try:
-				# Outer Tag Object
-				title = soup.find("span", attrs={"id": 'productTitle'})
+        # Title as a string value
+        title_string = title_value.strip().replace(",", "")
 
-				# Inner NavigableString Object
-				title_value = title.string
+    except AttributeError:
+        title_string = "NA"
+    print("product Title = ", title_string)
 
-				# Title as a string value
-				title_string = title_value.strip().replace(',', '')
+    # retrieving price
+    try:
+        price = (
+            soup.find("span", attrs={"class": "a-offscreen"})
+            .string.strip()
+            .replace(",", "")
+        )
+    except AttributeError:
+        price = "NA"
+    print("Product's price =", price)
 
-			except AttributeError:
-				title_string = "NA"
-			print("product Title = ", title_string)
+    # retrieving discount
+    try:
+        discount = (
+            soup.find(
+                "span",
+                attrs={
+                    "class": "a-size-large a-color-price savingPriceOverride aok-align-center reinventPriceSavingsPercentageMargin savingsPercentage"
+                },
+            )
+            .string.strip()
+            .replace("-", " ")
+        )
+    # we are omitting unnecessary spaces and commas from our string
+    except AttributeError:
+        discount = "NA"
+    print("Product's discount =", discount)
 
-			# retrieving price
-			try:
-				price = soup.find("span", attrs={'class': 'a-offscreen'}).string.strip().replace(',', '')
-			except AttributeError:
-				price = "NA"
-			print("Product's price =", price)
+    # retrieving product rating
+    try:
+        rating = (
+            soup.find("i", attrs={"class": "a-icon a-icon-star a-star-4-5"})
+            .string.strip()
+            .replace(",", "")
+        )
 
-			# retrieving discount
-			try:
-				discount = soup.find("span", attrs={'class': 'a-size-large a-color-price savingPriceOverride aok-align-center reinventPriceSavingsPercentageMargin savingsPercentage'}).string.strip().replace('-',' ')
-			# we are omitting unnecessary spaces and commas from our string
-			except AttributeError:
-				discount = "NA"
-			print("Product's discount =", discount)
+    except AttributeError:
+        try:
+            rating = (
+                soup.find("span", attrs={"class": "a-icon-alt"})
+                .string.strip()
+                .replace(",", "")
+            )
+        except:
+            rating = "NA"
+    print("Overall rating = ", rating)
 
-			# retrieving product rating
-			try:
-				rating = soup.find("i", attrs={'class': 'a-icon a-icon-star a-star-4-5'}).string.strip().replace(',', '')
+    try:
+        review_count = (
+            soup.find("span", attrs={"id": "acrCustomerReviewText"})
+            .string.strip()
+            .replace(",", "")
+        )
 
-			except AttributeError:
+    except AttributeError:
+        review_count = "NA"
+    print("Total reviews = ", review_count)
 
-				try:
-					rating = soup.find("span", attrs={'class': 'a-icon-alt'}).string.strip().replace(',', '')
-				except:
-					rating = "NA"
-			print("Overall rating = ", rating)
+    # print availablility status
+    try:
+        available = soup.find("div", attrs={"id": "availability"})
+        available = available.find("span").string.strip().replace(",", "")
 
-			try:
-				review_count = soup.find("span", attrs={'id': 'acrCustomerReviewText'}).string.strip().replace(',', '')
+    except AttributeError:
+        available = "NA"
+    print("Availability = ", available)
 
-			except AttributeError:
-				review_count = "NA"
-			print("Total reviews = ", review_count)
+    processing_df = pd.DataFrame(
+        [
+            [
+                product_name,
+                price,
+                discount,
+                rating,
+                review_count,
+                available,
+                datetime.now().date(),
+            ]
+        ],
+        columns=[
+            "Title",
+            "Price",
+            "Discount",
+            "Rating",
+            "Review Count",
+            "Availability",
+            "ExtractionDate",
+        ],
+    )
+    processing_df["ExtractionDate"] = pd.to_datetime(
+        processing_df["ExtractionDate"], errors="coerce"
+    ).dt.strftime("%Y-%m-%d")
+    return json.loads(processing_df.to_json(orient="records"))[0]
 
-			# print availablility status
-			try:
-				available = soup.find("div", attrs={'id': 'availability'})
-				available = available.find("span").string.strip().replace(',', '')
 
-			except AttributeError:
-				available = "NA"
-			print("Availability = ", available)
+async def flipkart_processing(product_name, headers, url):
+    page = requests.get(
+        url,
+        headers={
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
+        },
+    )
+    soup = BeautifulSoup(
+        page.content, "html.parser"
+    )  # it gives us the visual representation of data
 
-			processing_df = pd.DataFrame([[title_string, price, discount, rating, review_count, available]],
-										 columns=['Title', 'Price', 'Discount', "Rating", "Review Count", "Availability"])
+    # name of the product
+    try:
+        name = soup.find("span", class_="B_NuCI")
+        name = name.text.replace("   ", " ")
+    except AttributeError:
+        name = "NA"
+    print("Name: ", name)
 
-			return json.loads(processing_df.to_json(orient='records'))[0]
+    # the discounted price
+    try:
+        discPrice = soup.findAll(attrs={"class": "_25b18c"})[0].next.text
+    except AttributeError:
+        discPrice = "NA"
+    print("discPrice:", discPrice)
 
-	except Exception as e:
-		print(e)
+    # the original price
+    try:
+        product_price = [
+            x for x in soup.findAll(attrs={"class": "_25b18c"})[0].children
+        ][1].text
+    except AttributeError:
+        product_price = "NA"
+    print("original Price: ", product_price)
+
+    # calculating the percentage discount:
+    discount = (
+        (int(product_price.replace("₹", "")) - int(discPrice.replace("₹", "")))
+        / int(product_price.replace("₹", ""))
+    ) * 100
+    discount = round(discount, 1)
+    discount = str(discount) + "%"
+    print("discount:", discount)
+
+    # the average ratings(stars)
+    try:
+        stars = soup.find("div", class_="_2d4LTz")
+        stars = stars.text + " out of 5 stars"
+    except AttributeError:
+        stars = "NA"
+    print("ratings:", stars)
+
+    # the total ratings count(count)
+    try:
+        product_rating = re.findall(r"\d+,?\d+\sRatings", str(soup))[0]
+    except (AttributeError, IndexError):
+        product_rating = "NA"
+    print("ratings count:", product_rating)
+
+    # Item in stock check
+    try:
+        stock = soup.find("div", class_="_1dVbu9")
+        if stock is None:
+            stock = "Availability: In Stock"
+        else:
+            stock = "Availability:", stock.text
+    except AttributeError:
+        stock = "NA"
+        print(stock)
+
+    processing_df = pd.DataFrame(
+        [
+            [
+                product_name,
+                product_price,
+                discount,
+                stars,
+                product_rating,
+                stock,
+                datetime.now().date(),
+            ]
+        ],
+        columns=[
+            "Title",
+            "Price",
+            "Discount",
+            "Rating",
+            "Review Count",
+            "Availability",
+            "ExtractionDate",
+        ],
+    )
+    processing_df["ExtractionDate"] = pd.to_datetime(
+        processing_df["ExtractionDate"], errors="coerce"
+    ).dt.strftime("%Y-%m-%d")
+    return json.loads(processing_df.to_json(orient="records"))[0]
 
 
-def process_historical_data(product_name, url):
-	current_data = pd.read_json(single_product_data_fetcher(url))
-	df = pd.read_excel("Data/HistoricalData.xlsx")
-	print("")
+async def single_product_data_fetcher(product_name: str, url: dict) -> dict:
+    # to extract data from a script, we need to create a User-Agent as a has blocked robots from accessing data
+    user_agent = UserAgent()
+    headers = {"User-Agent": user_agent.random}
+    amazon_coroutine = amazon_processing(product_name, headers, url["amazon"])
+    flipkart_coroutine = flipkart_processing(product_name, headers, url["flipkart"])
+    amazon_result, flipkart_result = await asyncio.gather(amazon_coroutine, flipkart_coroutine)
+    processing_df = pd.DataFrame(
+        [
+            [
+                product_name,
+                amazon_result['Price'],
+                flipkart_result['Price'],
+                amazon_result['Discount'],
+                flipkart_result['Discount'],
+                amazon_result["Rating"],
+                flipkart_result["Rating"],
+                amazon_result["Review Count"],
+                flipkart_result["Review Count"],
+                amazon_result["Availability"],
+                datetime.now().date(),
+            ]
+        ],
+        columns=[
+            "Title",
+            "Amazon Price",
+            "Flipkart Price",
+            "Amazon Discount",
+            "Flipkart Discount",
+            "Amazon Rating",
+            "Flipkart Rating",
+            "Amazon Review Count",
+            "Flipkart Review Count",
+            "Availability",
+            "ExtractionDate",
+        ],
+    )
+    processing_df["ExtractionDate"] = pd.to_datetime(
+        processing_df["ExtractionDate"], errors="coerce"
+    ).dt.strftime("%Y-%m-%d")
+    return json.loads(processing_df.to_json(orient="records"))[0]
+
+
+async def process_historical_data(product_name, url):
+    product_data = await single_product_data_fetcher(product_name, url)
+    product_data_df = pd.DataFrame([product_data.values()], columns=product_data.keys())
+    all_historical_data_df = pd.read_excel("Data/HistoricalData.xlsx")
+    product_historical_df = all_historical_data_df[
+        all_historical_data_df["Title"] == product_name
+    ]
+    required_data = pd.concat([product_historical_df, product_data_df])
+    required_data["ExtractionDate"] = pd.to_datetime(
+        required_data["ExtractionDate"], errors="coerce"
+    ).dt.strftime("%Y-%m-%d")
+    return json.loads(required_data.to_json(orient="records"))
 
 
 def process_all_urls():
+    output = list()
 
-	output = list()
+    # opening our url file to access URLs
+    with open("Data/available_products.json", "r") as file_obj:
+        product_data = file_obj.read()
 
-	# opening our url file to access URLs
-	file = open("url.txt", "r")
+    # iterating over the urls
+    for product_name, product_urls in json.loads(product_data).items():
+        product_url = product_urls["amazon"]
+        output.append(single_product_data_fetcher(product_name, product_url))
 
-	# iterating over the urls
-	for links in file.readlines():
-		output.append(single_product_data_fetcher(links))
-
-	return output
+    return output
 
 
 if __name__ == "__main__":
-	process_historical_data("Vega Atom Helmet", "https://www.amazon.in/dp/B07RFQRF46")
-	pprint(process_all_urls(), indent=4)
+    process_historical_data("Vega Atom Helmet", "https://www.amazon.in/dp/B07RFQRF46")
+    pprint(process_all_urls(), indent=4)
