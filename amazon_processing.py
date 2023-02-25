4b6d0c733cdd334ad681fe7bd46e07d22df95147
ishmeet-grayopusq <120010191+ishmeet-grayopusq@users.noreply.github.com>
2023-02-25 23:00:41 +0530

    amazon fix

--- amazon_processing.py
+++ amazon_processing.py
@@ -10,7 +10,7 @@ async def amazon_processing(product_name, url):
     # Making the HTTP Request
     ua = UserAgent()
     headers = {
-        "User-Agent": ua.ie
+        "User-Agent": ua.msie
     }
     webpage = requests.get(url, headers=headers)
 
